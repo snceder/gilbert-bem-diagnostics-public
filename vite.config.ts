40ff151515859369300810fc0388a168dd3fb35e
snceder <admin@cedergrenlearning.com>
2026-08-25 12:42:59 -0500

    Remove base path from Vite configuration
    Removed the base path configuration from Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/gilbert-bem-diagnostics-public/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {

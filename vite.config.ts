11c29fbc84ad60c6319e28b138bfb4709f0e1dac
snceder <admin@cedergrenlearning.com>
2026-08-25 12:18:37 -0500

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/gilbert-bem-diagnostics-public/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
